14cc3b6a68ed1365997e0c0b76824d6a859defaf
Shivansh Tariyal <shivanshtariyal@gmail.com>
2023-06-13 22:54:13 +0530

    setup

---
+++ app/src/main/java/pro/shivanshtariyal/recipeapp/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package pro.shivanshtariyal.recipeapp.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
